3f75b7418ae59ac1b5e78186716cb3aa4bd330cb
Bernardo Lima <bernardo.lima.93@hotmail.com>
2023-02-18 17:30:34 -0300

    correcoes da funcao telefone

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const submit = {
   telefone: ''
 }
 
-
+// Validação de E-mail
 function validarEmail() {
   const email = document.getElementById("email").value;
   const validador = new ValidadorEmail(email);
@@ -27,31 +27,34 @@ function validarEmail() {
 
 // Validação de Telefone
 function validarTelefone() {
-    const telefone = document.getElementById("telefone").value;
-    const validador = new ValidadorTelefone(telefone);
-    const resultado = validador.validar();
-  
-    const mensagemElement = document.getElementById("mensagem");
-    if (resultado === true) {
-      mensagemElement.textContent = "Telefone válido.";
-    } else {
-      mensagemElement.textContent = resultado;
-    }
-  }
-  
-  class ValidadorTelefone {
-    constructor(telefone) {
-      this.telefone = telefone;
-    }
-  
-    validar() {
-      const regex = /^\(\d{2}\)\d{4}-\d{4}$/; // regex para verificar se o telefone está no formato correto
-      if (!regex.test(this.telefone)) {
-        return "Telefone inválido. O formato deve ser (NN)NNNN-NNNN.";
-      }
-      return this.telefone;
-    }
+  const telefone = document.getElementById("telefone").value;
+  const validador = new ValidadorTelefone(telefone);
+  const resultado = validador.validar();
 
+  const mensagemElement = document.getElementById("erroTelefone");
+  mensagemSubmit.textContent = "";
+  mensagemElement.textContent = "";
+  if (!resultado) {
+    mensagemElement.textContent = "Telefone inválido. O formato deve ser (NN)NNNN-NNNN.";
+  } else {
+    submit.telefone = telefone;
+    if (estaPreenchido())
+      document.getElementById('botao-submit').disabled = false;
+  }
+}
+  
+class ValidadorTelefone {
+  constructor(telefone) {
+    this.telefone = telefone;
+  }
+
+  validar() {
+    const regex = /^\(\d{2}\)\d{4}-\d{4}$/; // regex para verificar se o telefone está no formato correto
+    if (!regex.test(this.telefone)) {
+      return false
+    }
+    return true
+  }
 }
 class ValidadorEmail {
   constructor(email) {
