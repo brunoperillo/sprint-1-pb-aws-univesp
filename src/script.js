21d25ab539c7f3e62baa227985b1d75f9a6ffe49
Ana Vitória Louro Navili <va014863@gmail.com>
2023-02-18 10:24:16 -0300

    ajustes estéticos na mensagem de erro

---
+++ src/script.js
@@ -0,0 +1,32 @@
+// Validação de Telefone
+function validarTelefone() {
+    const telefone = document.getElementById("telefone").value;
+    const validador = new ValidadorTelefone(telefone);
+    const resultado = validador.validar();
+  
+    const mensagemElement = document.getElementById("mensagem");
+    if (resultado === true) {
+      mensagemElement.textContent = "Telefone válido.";
+    } else {
+      mensagemElement.textContent = resultado;
+    }
+  }
+  
+  class ValidadorTelefone {
+    constructor(telefone) {
+      this.telefone = telefone;
+    }
+  
+    validar() {
+      const regex = /^\(\d{2}\)\d{4}-\d{4}$/; // regex para verificar se o telefone está no formato correto
+      if (!regex.test(this.telefone)) {
+        return "Telefone inválido. O formato deve ser (NN)NNNN-NNNN.";
+      }
+      return this.telefone;
+    }
+  }
+  
+  
+  
+  
+  
